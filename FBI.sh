88670c9f9b2d4078758e62227c9c6132e8833ef7
fouber <fouber.NET@gmail.com>
2016-02-02 12:35:15 +0800

    Create FBI.sh

---
+++ FBI.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BRL='\033[41m'
+BRR='\033[49m'
+CWL='\033[37m'
+CWR='\033[39m'
+
+printf "\n"
+printf "                      ${BRL} FBI WARNING ${BRR}\n"
+printf "\n${CWL}"
+printf "Federal Law provides severe civil and criminal penalties for \n"
+printf "the unauthorized reproduction, distribution, or exhibition of \n"
+printf "copyrighted motion pictures (Title 17, United States Code, \n"
+printf "Sections 501 and 508). The Federal Bureau of Investigation \n"
+printf "investigates allegations of criminal copyright infringement\n"
+printf "        (Title 17, United States Code, Section 506).\n"
+printf "${CWR}\n"
