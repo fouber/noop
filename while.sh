85cf69e27b2aa500be1583db8b3aff8acf63523c
fouber <fouber.NET@gmail.com>
2016-02-02 17:00:03 +0800

    Update while.sh

--- while.sh
+++ while.sh
@@ -4,5 +4,5 @@ while true
 do
     pppoe-stop 2>&1 1>/dev/null
     pppoe-start 2>&1 1>/dev/null
-    wget -qO- http://1212.ip138.com/ic.asp | grep body | awk -F '\[|\]' '{print "[D] IP="$2}'
+    wget -qO- http://1212.ip138.com/ic.asp | grep body | awk -F '\[|\]' '{print "[D] IP="$2" `date +%H:%M:%S`"}'
 done
