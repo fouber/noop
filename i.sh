07548412394a229add7962b5efa48b063b811c66
fouber <fouber.NET@gmail.com>
2016-02-16 16:22:36 +0800

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse" > /etc/apt/sources.list
 echo "deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse" >> /etc/apt/sources.list
@@ -19,4 +19,4 @@ WORKDIR=/tmp
 
 cd ${WORKDIR}
 git clone https://github.com/fouber/spam.git
-sh ${WORKDIR}/spam/bin/init
+sh ${WORKDIR}/spam/bin/init ${WORKDIR}/spam
