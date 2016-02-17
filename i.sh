003d89e9136c8f717497fe7e5a8fe8e774a343f0
fouber <fouber.NET@gmail.com>
2016-02-17 15:44:55 +0800

    Update i.sh

--- i.sh
+++ i.sh
@@ -12,7 +12,8 @@ fetch(){
 }
 
 # dns
-fetch resolv.conf /etc
+echo "nameserver 223.5.5.5"  > /etc/resolv.conf
+echo "nameserver 223.6.6.6" >> /etc/resolv.conf
 
 # timezone
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
