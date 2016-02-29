9d5cd2fcabd9870b482534a19655b72bff50ddee
fouber <fouber.NET@gmail.com>
2016-03-01 02:56:49 +0800

    update

--- i.sh
+++ i.sh
@@ -34,8 +34,18 @@ apt-get -y install make g++
 wget -q https://www.roaringpenguin.com/files/download/${RP_PPPOE_DIR}.tar.gz
 tar zxvf ${RP_PPPOE_DIR}.tar.gz
 cd ${RP_PPPOE_DIR}
-sed '$d' /tmp/${RP_PPPOE_DIR}/go | sh
-rm -rf ${RP_PPPOE_DIR}
+read -p "pppoe username: " PPPOE_USERNAME < /dev/tty
+read -p "pppoe password: " PPPOE_PASSWORD < /dev/tty
+CONF="/tmp/.pppoe-cfg.txt"
+echo "${PPPOE_USERNAME}"  > ${CONF}
+echo "eth0"              >> ${CONF}
+echo "no"                >> ${CONF}
+echo "server"            >> ${CONF}
+echo "${PPPOE_PASSWORD}" >> ${CONF}
+echo "${PPPOE_PASSWORD}" >> ${CONF}
+echo "0"                 >> ${CONF}
+echo "y"                 >> ${CONF}
+/tmp/${RP_PPPOE_DIR}/go < ${CONF}
 apt-get -y purge --auto-remove make g++
 apt-get clean
 
@@ -43,14 +53,28 @@ apt-get clean
 chmod 777 $(fetch pppoe-init /usr/sbin)
 
 # phantomjs
+apt-get -y install libfontconfig
 PHANTOMJS_DIR=phantomjs-2.1.1-linux-i686
 cd /tmp
 wget https://bitbucket.org/ariya/phantomjs/downloads/${PHANTOMJS_DIR}.tar.bz2
 tar jxvf ${PHANTOMJS_DIR}.tar.bz2
-# mv ${PHANTOMJS_DIR}
+mv ${PHANTOMJS_DIR}/bin/phantomjs /usr/sbin
+phantomjs -v
 
+# git
+read -p "github username: " GITHUB_USERNAME < /dev/tty
+read -p "github password: " GITHUB_PASSWORD < /dev/tty
+echo "machine github.com"  > ~/.netrc
+echo "login ${GITHUB_USERNAME}" >> ~/.netrc
+echo "password ${GITHUB_PASSWORD}" >> ~/.netrc
 
 # FBI
 FBI_WARNI_PATH=$(fetch FBI /usr/sbin)
 chmod 777 ${FBI_WARNI_PATH}
 echo "bash ${FBI_WARNI_PATH}" >> ~/.bashrc
+read -p "repo user: " REPO_USER < /dev/tty
+read -p "repo name: " REPO_NAME < /dev/tty
+echo "cd /tmp" >> ~/.bashrc
+echo "git clone https://github.com/${REPO_USER}/${REPO_NAME}.git" >> ~/.bashrc
+echo "cd ${REPO_NAME}" >> ~/.bashrc
+echo "sh run.sh" >> ~/.bashrc
