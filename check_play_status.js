db86fa4ee19a5a39015ea658b69fb0bf273f913e
fouber <fouber.NET@gmail.com>
2016-07-24 11:40:32 +0800

    upd

--- check_play_status.js
+++ check_play_status.js
@@ -1,19 +1,29 @@
 (function(){
     var import = function(lib, callback, check){
         if(check && window.hasOwnProperty(check)){
-            callback();
+            callback(window[check]);
         } else {
             var url = '//fouber.github.com/noop/lib/' + lib + '.min.js';
             var script = document.createElement('script');
-            script.onload = callback;
+            script.onload = check ? function(){
+                callback(window[check]);
+            } : callback;
             script.src = url;
-            document.head.appendChild();
+            document.head.appendChild(script);
         }
     };
     var path = location.pathname;
     var id = path.match(/^\/(v|star)\/(\w+)/)[2];
     if(id){
-        
+        import('zepto', function($){
+            var fetch = function(callback){
+                var url = '/json/rooms/' + id + '/info4.json?t=' + Date.now();
+                $.get(url, function(data){
+                    console.log(data);
+                });
+            };
+            fetch();
+        }, 'Zepto');
     } else {
         alert('请在直播间使用');
     }
