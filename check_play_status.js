9d172809f9a4ba9bf4d3f16ddba34479ea1348cf
fouber <fouber.NET@gmail.com>
2016-07-24 11:07:00 +0800

    upd

--- check_play_status.js
+++ check_play_status.js
@@ -1,7 +1,20 @@
 (function(){
-  var path = location.pathname;
-  var id = path.match(/^\/(v|star)\/(\w+)/)[2];
-  if(id){
-    alert(id);
-  }
+    var import = function(lib, callback, check){
+        if(check && window.hasOwnProperty(check)){
+            callback();
+        } else {
+            var url = '//fouber.github.com/noop/lib/' + lib + '.min.js';
+            var script = document.createElement('script');
+            script.onload = callback;
+            script.src = url;
+            document.head.appendChild();
+        }
+    };
+    var path = location.pathname;
+    var id = path.match(/^\/(v|star)\/(\w+)/)[2];
+    if(id){
+        
+    } else {
+        alert('请在直播间使用');
+    }
 })();
